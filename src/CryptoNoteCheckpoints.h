24b85d55a233d057a9fb77baaa9efbed893b40f7
fredenrg <39649555+fredenrg@users.noreply.github.com>
2018-09-22 20:07:30 +0400

    Update CryptoNoteCheckpoints.h

--- src/CryptoNoteCheckpoints.h
+++ src/CryptoNoteCheckpoints.h
@@ -30,6 +30,6 @@ const std::initializer_list<CheckpointData> CHECKPOINTS = {
 {     0, "16f31d36d64f8b029f1c7f3b5ddb36f010666325eaee86c3b0bbb6af826d3703"},
 {   500, "b94e4dd1d370a6d51bd2b36296674391bb0ba63620abf102a3dbcc37fa2d5b74"},
 {  1000, "ba88f0a1de444fefbbde9d8c815b866b1e48d69c7cc8a1cca44ed87d799de08f"},
-{  1900, "d13ffcd4dab1bd760a8a0abdc146db5730efb3fd5d8ca7fe6feb6e9c94ec4cd1"}
+{  3000, "cf47151fe2006cc8bd4451ce7cfa44db9e502b355be46d1a637b59f1d5581700"}
 };
 }
