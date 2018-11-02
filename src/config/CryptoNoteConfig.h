6a7adbb376cc87c699cd8ef284caa9d70ced9a79
fredenrg <39649555+fredenrg@users.noreply.github.com>
2018-11-02 08:41:25 +0400

    Fork height correction

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -134,10 +134,9 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
-    31167,  // 0
-    31189,  // 1
-    90000,  //2
-    100000,  //3
+    31189,  // 0
+    90000,  // 1
+    100000,  //2
 };
 
 /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
