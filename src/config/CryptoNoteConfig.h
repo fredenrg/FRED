8da1c608f14a2916613e01b6617cdbcde98d8169
fredenrg <39649555+fredenrg@users.noreply.github.com>
2018-11-02 18:41:19 +0400

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -134,7 +134,7 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
-    31189,  // 0
+    31198,  // 0
     90000,  // 1
     100000,  //2
 };
