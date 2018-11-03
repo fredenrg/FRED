f8cb4757da16ef1a39f2520877567da559ef2619
fredenrg <39649555+fredenrg@users.noreply.github.com>
2018-11-03 16:49:38 +0400

    Ports, CN Light height change

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -122,7 +122,7 @@ const uint32_t KEY_IMAGE_CHECKING_BLOCK_INDEX                = 0;
 
 const uint32_t UPGRADE_HEIGHT_V2                             = 1;
 const uint32_t UPGRADE_HEIGHT_V3                             = 2;
-const uint32_t UPGRADE_HEIGHT_V4                             = 60000; // Upgrade height for CN-Lite Variant 1 switch.
+const uint32_t UPGRADE_HEIGHT_V4                             = 80000; // Upgrade height for CN-Lite Variant 1 switch.
 const uint32_t UPGRADE_HEIGHT_CURRENT                        = UPGRADE_HEIGHT_V4;
 
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               // percent
@@ -134,7 +134,7 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
-    60000,  // 0
+    80000,  // 0
     90000,  // 1
     140000,  //2
 };
@@ -181,9 +181,9 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  31806;
-const int      RPC_DEFAULT_PORT                              =  31807;
-const int      SERVICE_DEFAULT_PORT                          =  8754;
+const int      P2P_DEFAULT_PORT                              =  32808;
+const int      RPC_DEFAULT_PORT                              =  32809;
+const int      SERVICE_DEFAULT_PORT                          =  8070;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
@@ -211,14 +211,14 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 const char     LATEST_VERSION_URL[]                          = "https://github.com/fredenrg/FRED/releases";
 const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 {
-    {  0xf5, 0x4c, 0x34, 0x6f, 0xcd, 0x51, 0x54, 0x27, 0x35, 0xf8, 0x41, 0xb4, 0xa6, 0xd1, 0x63, 0xf9  }
+    {  0xf7, 0x5c, 0x44, 0x6f, 0xcd, 0x51, 0x54, 0x27, 0x35, 0xf8, 0x41, 0xb4, 0xa6, 0xd1, 0x63, 0xf9  }
 };
 
 const char* const SEED_NODES[] = {
-  "159.89.121.156:31806", //seed1
-  "142.93.81.195:31806", //seed2
-  "165.227.37.180:31806", //seed3
-  "142.93.199.193:31806", //Pseed4
-  "167.99.181.195:31806" //Exp
+  "159.89.121.156:32808", //seed1
+  "142.93.81.195:32808", //seed2
+  "165.227.37.180:32808", //seed3
+  "142.93.199.193:32808", //Pseed4
+  "167.99.181.195:32808" //Exp
 };
 } // CryptoNote
