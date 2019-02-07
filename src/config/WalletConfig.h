0c5a44f5ccad1e3f681b8ea5f10677674e22ad76
fredenrg <39649555+fredenrg@users.noreply.github.com>
2019-02-07 16:14:42 +0400

    Fix the mix

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -65,8 +65,8 @@ namespace WalletConfig
     const uint64_t minimumSend = 100000;
 
     /* Is a mixin of zero disabled on your network? */
-    const bool mixinZeroDisabled = true;
-    const uint64_t mixinZeroDisabledHeight = 180000;
+    const bool mixinZeroDisabled = false;
+    const uint64_t mixinZeroDisabledHeight = 300000;
 
     /* If a mixin of zero is disabled, at what height was it disabled? E.g.
        fork height, or 0, if never allowed. This is ignored if a mixin of
